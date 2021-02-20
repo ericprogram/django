17b8758faea1fa26f87c530ae6a8a5a5d39e53bf
hmchen <hmchen@cenboom.com>
2021-02-20 23:01:48 +0800

    itsdangerous 对openid的加密，解密

---
+++ meiduo_project/meiduo_mall/meiduo_mall/apps/oauth/utils.py
@@ -0,0 +1,32 @@
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, BadData
+from django.conf import settings
+
+
+def generate_openid_signature(openid):
+    """ 对openid进行加密 """
+    # 1.创建加密示例对象
+    serializer = Serializer(secret_key=settings.SECRET_KEY, expires_in=600)
+
+    # 2.包装要加密的数据{}
+    date ={'openid': openid}
+
+    # 3.调用dumps方法进行加密 bytes
+    openid_bytes = serializer.dumps(date)  # 序列化(模型转换成字典) --> 输出
+
+    # 4.加bytes转换为str类型
+    return openid_bytes.decode()
+
+
+def check_openid(openid_sign):
+    """对openid进行解密"""
+    # 1.创建加密示例对象
+    serializer = Serializer(secret_key=settings.SECRET_KEY, expires_in=600)
+    try:
+        # 2.对poen进行解密
+        data = serializer.loads(openid_sign)  # 反序列化（字典转换为模型） -->输入
+        # 3. 返回openid
+        return data.get('openid')
+    except BadData:
+        return None
+
+
